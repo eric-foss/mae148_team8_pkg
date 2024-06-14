23f2d8f7a84c73b607d9dfa64a887998137c5bea
AJMarsh-Eng <146989549+AJMarsh-Eng@users.noreply.github.com>
2024-06-14 16:06:00 -0700

    Update gps_node.py

--- mae148_team8_pkg/gps_node.py
+++ mae148_team8_pkg/gps_node.py
@@ -10,6 +10,7 @@ from scripts.coord_handling import initialize_wksp_obs,LonLat_To_XY,XY_To_LonLat
 import shapely
 from scripts.transform import PIDController
 from scripts.path import CTE
+from scripts.steering_guidance import getzangrot
 
 
 class GPSNode(Node):
@@ -17,6 +18,11 @@ class GPSNode(Node):
     servo_command = Int32()
     servo_command.data = random.randint(0,3)
     servo_message = ''
+    startmov_bool=False
+    initial_bool=True
+    stop_drop_roll_bool=False
+    speed = Twist()
+    counter=0
 
     
     if servo_command.data == 0:
@@ -65,19 +71,50 @@ class GPSNode(Node):
         self.vesc_publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
 
         #Success Publisher
-        self.servo_publisher_ = self.create_publisher(Int32,'pathing_complete',10)
+        self.servo1_publisher_ = self.create_publisher(Bool,'servo1',10)
         self.get_logger().info(self.servo_message)
 
-    def gps_callback(self, msg):
-        self.lat = msg.latitude
-        self.long = msg.longitude
-        self.alt = msg.altitude
-        self.get_logger().info(f'Latitude: {self.lat:.2f}, Longitude: {self.long:.2f}, Altitude: {self.alt:.2f}')
-
+        self.servo2_publisher_ = self.create_publisher(Bool,'servo2',10)
+        self.get_logger().info(self.servo_message)
     
+    def hitch_callback(self,msg):
+        self.get_logger().info("Successful latch. Begin movement.")
+        self.startmov_bool=True
+
+
+    def gps_callback(self, msg):
+        #location list:
+        #eb1:(32.881275,-117.235466)
+        #geisel steps:(32.881118,-117.235901)
+        #
+        goal_loc=(32.881118,-117.235901)
+        if self.startmov_bool and not self.stop_drop_roll_bool:
+            self.counter+=1
+            lat = msg.latitude
+            long = msg.longitude
+            alt=msg.altitude
+            self.get_logger().info(f'Latitude: {lat:.2f}, Longitude: {long:.2f}, Altitude: {alt:.2f}')
+            currPoi=LonLat_To_XY(long,lat)
+            if self.initial_bool:
+                self.initial_bool=False
+                start_loc=shapely.Point(currPoi)
+                our_ws=self.getPath(self,start_loc,goal_loc)
+                path=our_ws.path_coords
+            zang=getzangrot(path,currPoi,self.PID,self.CTE)
+            if our_ws.isNearGoal(currPoi):
+                self.speed.linear.x=0
+                self.speed.angular.z=0
+                self.stop_drop_roll_bool=True
+                self.servo1_publisher_.publish(True)
+                self.servo2_publisher_.publish(True)
+            else:
+                self.speed.linear.x=1
+                self.speed.angular.z=zang
+            self.speed_publisher_.publish(self.speed)
+
     def getPath(self,start_location,goal_location):
         ws = Workspace(self.workspace,self.obs_dict,start_location,goal_location,boundary_type = 1)
-        return ws.path_coords
+        return ws
     
 
 
