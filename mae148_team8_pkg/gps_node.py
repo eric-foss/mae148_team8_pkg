9a45ec1f82046a862b91406fb55356342e0253cd
AJMarsh-Eng <146989549+AJMarsh-Eng@users.noreply.github.com>
2024-06-15 14:18:47 -0700

    Update gps_node.py

--- mae148_team8_pkg/gps_node.py
+++ mae148_team8_pkg/gps_node.py
@@ -8,12 +8,12 @@ from rclpy.qos import ReliabilityPolicy, QoSProfile
 #import sys, os
 #sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'scripts'))
 #sys.path.append(os.path.join(sys.path[0], 'scripts'))
-from workspace3 import Workspace
-from coord_handling import initialize_wksp_obs,LonLat_To_XY,XY_To_LonLat
+from .workspace3 import Workspace
+from .coord_handling import initialize_wksp_obs,LonLat_To_XY,XY_To_LonLat
 import shapely
-from transform import PIDController
-from path import CTE
-from steering_guidance import getzangrot
+from .transform import PIDController
+from .path import CTE
+from .steering_guidance import getzangrot
 
 
 class GPSNode(Node):
